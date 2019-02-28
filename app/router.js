4297ef7a44186a45b3e15ac57c031992e933a64c
GabrielStahlberg <gabrielstahlberg@gmaill.com>
2019-02-27 23:11:12 -0300

    Criação da aplicação, primeiros arquivos de template e declaração de algumas rotas

--- app/router.js
+++ app/router.js
@@ -7,6 +7,17 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  /**
+   * this.route('about'); a linha abaixo poderia ser assim, pelo fato de terem a mesma nomenclatura.
+  */
+  this.route('about', {path:'about'});
+  this.route('services');
+  this.route('contact');
+
+  this.route('posts', {path: '/posts'}, function() {
+    this.route('new');
+    this.route('post', {path: ':post_id'});
+  });
 });
 
 export default Router;
